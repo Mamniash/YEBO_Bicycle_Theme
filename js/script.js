728eb2965c1ea44ebb5afb302dbac2421402c2da
Mamniash <mamniashvili2003.com>
2023-08-12 11:53:36 +0300

    starting

---
+++ js/script.js
@@ -0,0 +1,45 @@
+document.querySelector('.icon-menu').addEventListener('click', function () {
+   this.classList.toggle('active');
+   document.querySelector('.menu__body').classList.toggle('active');
+   document.querySelector('body').classList.toggle('lock');
+   console.log('sucsecc');
+});
+
+// console.log(document.querySelector('.slider__body').length);
+
+// if (document.querySelector('.slider__body').length > 0) {
+//    console.log('awhfiriergrthjftrhtj');
+//    document.querySelector('.slider__body').slick({
+//       dots: true,
+//       arrows: false,
+//       accessibility: false,
+//       slidesToShow: 1,
+//       autoplaySpeed: 2600,
+//       nextArrow: '<button type="button" class="slick-next"></button>',
+//       prevArrow: '<button type="button" class="slick-prev"></button>',
+//       responsive: [{
+//          breakpoint: 768,
+//          settings: {}
+//       }]
+//    });
+// }
+
+new Swiper('.swiper-container', {
+   pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+   },
+});
+
+
+function ibg() {
+
+   let ibg = document.querySelectorAll(".ibg");
+   for (var i = 0; i < ibg.length; i++) {
+      if (ibg[i].querySelector('img')) {
+         ibg[i].style.backgroundImage = 'url(' + ibg[i].querySelector('img').getAttribute('src') + ')';
+      }
+   }
+}
+
+ibg();
